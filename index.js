5fba290d0fdbf6db22d72bf1f5c855dc751c80cc
asms54 <lavishlook00@gmail.com>
2023-04-12 03:39:27 +0500

    Question 2

--- index.js
+++ index.js
@@ -8,4 +8,29 @@
 // let adds = add(5);
 // adds(8);
 
+//QUESTION NO. 2
+
+
+
+// let checkout = 0;
+//     let numArray = ["1","2","3","4","5","6"];
+//     newNumber = prompt("Enter value to check in array:");
+//     checkArray(newNumber);
+//     function checkArray(number) {
+//         for (let index = 0, check = 0; index < numArray.length && check==0; index++) {
+//             if (number == numArray[index]) {
+//                 check = 1;
+//                 checkout = check;
+//                 console.log("true");
+//             }
+//         }
+//         if (checkout == 0) {
+//             console.log("false");
+//             newNumber = prompt("Number is'nt present. Please enter new value:")
+//             checkArray(newNumber);
+//         }
+//     }
+
+
+
 
